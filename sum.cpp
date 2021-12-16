e5136d827ba80708ae3d0685740cc0bef527a232
Gaurav Tembhare <gauravtembhare07@gmail.com>
2021-12-16 22:07:10 +0530

    initial commit

---
+++ sum.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1;
+    cout<<"enter the first no. :-"<<endl;
+    cin>>num1;
+    
+
+    int num2;
+    cout<<"enter the second no. :-"<<endl;
+    cin>>num2;
+
+    int sum= num1 + num2;
+
+    cout<<"The sum is:-"<<sum<<endl; 
+    
+
+    return 0;
+}
